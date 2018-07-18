4a60b7c5cb4d0d82d8cd14a34d183e5bcec77272
kylepecci <kyle.pecci@gmail.com>
2018-07-18 20:16:04 +0100

    Styling Commit

--- public/insults.js
+++ public/insults.js
@@ -1,14 +1,12 @@
-document
-    .querySelector(".request-insult")
-    .addEventListener("click", function(){
-        fetch("/insult")
-        .then(function(res){
-            return res.json();
-        })
-        .then(function(data){
-            document.querySelector(".insult").innerText = data.compliment;
-        })
-        .catch(function(err) {
-            console.error(err);
-        })
+document.querySelector(".request-insult").addEventListener("click", function() {
+  fetch("/insult")
+    .then(function(res) {
+      return res.json();
     })
+    .then(function(data) {
+      document.querySelector(".insult").innerText = data.compliment;
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+});
--- server.js
+++ server.js
@@ -2,53 +2,55 @@ const express = require("express");
 const path = require("path");
 
 const compliments = [
-    "You look nice today",
-    "That dress looks nice on you",
-    "Have you been working out?",
-    "You can do hard things",
-    "You've gotten far in this course. You're really smart",
-    "You're programming! How cool is that?",
-    "I'm really proud of you",
-    "You made this",
-    "You've learned a lot of things, and that's pretty hard to do"
+  "You look nice today",
+  "That dress looks nice on you",
+  "Have you been working out?",
+  "You can do hard things",
+  "You've gotten far in this course. You're really smart",
+  "You're programming! How cool is that?",
+  "I'm really proud of you",
+  "You made this",
+  "You've learned a lot of things, and that's pretty hard to do"
 ];
 
 const insults = [
-    "No one loves you",
-    "You're ugly",
-    "You will die alone",
-    "Donald Trump is a better person than you",
-    "Dumbest person I've ever met",
+  "You don't look nice today",
+  "That dress doesn't looks nice on you",
+  "When was the last time you worked out?",
+  "You haven't gotten far in this course. You're not smart",
+  "You're not programming! How lame is that?",
+  "I'm not proud of you",
+  "You haven't learned a lot of things, and that's not hard to do"
 ];
 
-
-function getRandomCompliment(){
-    const randomIndex = Math.floor(Math.random() * compliments.length);
-    return compliments[randomIndex];
+function getRandomCompliment() {
+  const randomIndex = Math.floor(Math.random() * compliments.length);
+  return compliments[randomIndex];
 }
 
-function getRandomInsult(){
-    const randomIndex = Math.floor(Math.random() * insults.length);
-    return insults[randomIndex];
+function getRandomInsult() {
+  const randomIndex = Math.floor(Math.random() * insults.length);
+  return insults[randomIndex];
 }
 
 const app = express();
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-
-app.get("/compliment", function(req, res){
-    res.json({
-        compliment: getRandomCompliment()
+app.get("/compliment", function(req, res) {
+  res
+    .json({
+      compliment: getRandomCompliment()
     })
     .end();
 });
 
-app.get("/insult", function(req, res){
-    res.json({
-        compliment: getRandomInsult()
+app.get("/insult", function(req, res) {
+  res
+    .json({
+      compliment: getRandomInsult()
     })
     .end();
 });
@@ -56,11 +58,9 @@ app.get("/insult", function(req, res){
 app.use("/public", express.static("./public"));
 
 var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function(){
-    console.log("listening on Port 3000");
-
+app.listen(port, "0.0.0.0", function() {
+  console.log("listening on Port 3000");
 });
-
 
 /////// Creating basic server \\\\\\\\
 // const http = require("http");
